98a9dff292128d04449a597bc0b70efa4a72c39b
Saptarshi Neogi <neogisaptarshi@gmail.com>
2022-10-18 20:14:13 +0530

    Update Experiment_1.m

--- Experiment_1.m
+++ Experiment_1.m
@@ -4,26 +4,18 @@ alpha1=1;
 alpha2=2;
 alpha3=3;
 alpha4=4;
-alpha5=1354;
+alpha5=1268;
 
 n1=1.5;
-n2=1.45
+n2=1.45;
 a=25;
 delta=((n1)^2-(n2)^2)/(2*(n1)^2);
-nr1=[];
-nr2=[];
-nr3=[];
-nr4=[];
-nr5=[];
-nr6=[];
-nr7=[];
-nr8=[];
-nr9=[];
-nr10=[];
+nr1=[]; nr2=[]; nr3=[]; nr4=[]; nr5=[];
+nr6=[]; nr7=[]; nr8=[]; nr9=[];nr10=[];
 nr11=[];nr22=[];nr33=[];nr44=[];nr55=[];
 
 for r=1:40
-    if r<(a-1)
+    if r<(a)
         
         nr1(r)=n1*(1-2*delta*((r/a)^alpha1))^0.5;
         nr3(r)=n1*(1-2*delta*((r/a)^alpha2))^0.5;
@@ -52,19 +44,18 @@ nr55=[nr10 1.5 nr9];
 
 radius=-40:1:40;
 %radius=1:1:250;
-plot(radius,nr11,'r');
+plot(radius,nr11,'r','linewidth',2);
 hold on;
-plot(radius,nr22,'b');
+plot(radius,nr22,'b','linewidth',2);
 hold on;
-plot(radius,nr33,'g');
+plot(radius,nr33,'g','linewidth',2);
 hold on;
-plot(radius,nr44,'c');
+plot(radius,nr44,'c','linewidth',2);
 hold on;
-plot(radius,nr55,'m');
+plot(radius,nr55,'m','linewidth',2);
 xlabel('Radial distance in micrometer');
 ylabel('Refractive index profile');
-
 ylim([1.43 1.53]);
-
-legend('alpha=1','alpha=2','alpha=3','alpha=4','alpha=1354');
+legend('alpha=1','alpha=2','alpha=3','alpha=4','alpha=1268');
+title('Refractive Index Profile over Radial Distance for Graded Index Fiber');
 grid on;
